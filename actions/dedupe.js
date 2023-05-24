7d95649970a6463ca7f72431e1cbf58a527f26b4
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-05-24 12:39:53 -0600

    Refactor for DeDupe
    - Refactor of Dedupe code included

--- actions/dedupe.js
+++ actions/dedupe.js
@@ -1,57 +1,32 @@
-import { pipe } from 'fp-ts/function';
-import * as RA from 'fp-ts/ReadonlyArray';
-import * as RR from 'fp-ts/ReadonlyRecord';
-import * as O from 'fp-ts/Option';
-import * as E from 'fp-ts/Either';
-import * as D from 'io-ts/Decoder';
-
-/**
- * Calculates the unique records and builds up a list of ids to be removed.
- *
- * @param records - Inbound records from the FF API response
- * @returns A list of record ids to be removed
- */
 export const dedupeEmployees = (records) => {
-  let uniques = {}; // Dictionary to store unique records
-  let removeThese = []; // Array to store record IDs to be removed
+  const uniques = {}; // Object to store unique records based on employeeId
+  const removeThese = []; // Array to store record IDs to be removed
 
-  pipe(
-    records,
-    RA.map((record) => {
-      const decodedValue = D.string.decode(record.values.employeeId.value); // Decode the employeeId value using io-ts Decoder
+  for (const record of records) {
+    const employeeId = record.values.employeeId.value; // Get the employeeId value from the record
 
-      return pipe(
-        decodedValue,
-        E.fold(
-          (decodingErrors) => {
-            console.log('Decoding error:', decodingErrors);
-            // Handle decoding error if necessary
-          },
-          (employeeId) => {
-            pipe(
-              RR.lookup(employeeId)(uniques), // Looks up the key in the dictionary
-              O.match(
-                () => {
-                  uniques = RR.upsertAt(employeeId, record)(uniques); // Adds the record to the dictionary if the key is not found
-                },
-                (latestRecord) => {
-                  if (
-                    record.values.hireDate.value >
-                    latestRecord.values.hireDate.value
-                  ) {
-                    removeThese = RA.append(latestRecord.id)(removeThese); // Appends the ID of the existing record to the removal list
-                    uniques = RR.upsertAt(employeeId, record)(uniques); // Updates the dictionary with the new record
-                  } else {
-                    removeThese = RA.append(record.id)(removeThese); // Appends the ID of the current record to the removal list
-                  }
-                }
-              )
-            );
-          }
-        )
-      );
-    })
-  );
+    if (typeof employeeId === 'string') {
+      // Check if the employeeId is of type string
+      if (!(employeeId in uniques)) {
+        // If the employeeId is not found in the uniques object, add the record as a unique record
+        uniques[employeeId] = record;
+      } else {
+        // If the employeeId is already in the uniques object, compare hire dates to determine which record to keep
+        const latestRecord = uniques[employeeId];
+
+        if (record.values.hireDate.value > latestRecord.values.hireDate.value) {
+          // If the current record has a later hire date, remove the latestRecord and add the current record
+          removeThese.push(latestRecord.id);
+          uniques[employeeId] = record;
+        } else {
+          // If the current record has an earlier or equal hire date, add the current record to the removal list
+          removeThese.push(record.id);
+        }
+      }
+    } else {
+      console.log('Invalid employeeId:', employeeId); // Log an error message for invalid employeeId values
+    }
+  }
 
   return removeThese; // Return the list of record IDs to be removed
 };
