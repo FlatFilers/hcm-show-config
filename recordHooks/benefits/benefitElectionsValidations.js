f485e619cf008a5935ade3f68806133f4a56a3de
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-13 11:14:30 -0600

    Removes custom functions for Dates and Numbers
    - removes dateFormats, dateFormatting, and validateNumberFields files
    - removes references across all recordHooks files

--- recordHooks/benefits/benefitElectionsValidations.js
+++ recordHooks/benefits/benefitElectionsValidations.js
@@ -1,25 +1,9 @@
-import { formatRecordDates } from '../../common/dateFormatting';
-import { validateNumberFields } from '../../common/validateNumberFields';
 import { roundEmployerContribution } from './roundEmployerContribution';
 
 export function employeeValidations(record) {
   // Validate the input record parameter
   if (!record || typeof record !== 'object') {
     throw new Error('Invalid record input. Expecting a valid record object.');
-  }
-
-  try {
-    formatRecordDates(record, 'benefit-elections-sheet');
-  } catch (error) {
-    console.log('Error occurred during date formatting:', error);
-    // Handle or rethrow the error as needed
-  }
-
-  try {
-    validateNumberFields(record, 'benefit-elections-sheet');
-  } catch (error) {
-    console.log('Error occurred during number field validation:', error);
-    // Handle or rethrow the error as needed
   }
 
   try {
