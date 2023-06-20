0e44629e42a2aea28611dfe49f7c21ad2979db23
mhpjay422 <12041898+mhpjay422@users.noreply.github.com>
2023-06-20 12:15:51 -0400

    update filefeed config with theme and document (#48)

--- workflows/filefeed/index.ts
+++ workflows/filefeed/index.ts
@@ -23,7 +23,6 @@ export default function (listener) {
 
     post({
       hostname: 'hcm.show',
-      // hostname: 'b79e-108-27-27-221.ngrok-free.app',
       path: '/api/v1/sync-file-feed',
       body: { spaceId, topic },
     });
@@ -31,61 +30,170 @@ export default function (listener) {
 
   // Add an event listener for the 'job:created' event
   listener.filter({ job: 'space:configure' }, (configure) => {
-    console.log('on space:configure', configure);
-
-    // Add an event listener for the 'job:created' event with a filter on 'space:configure'
     configure.on('job:ready', async (event) => {
-      // Log the event object as a JSON string to the console
-      console.log(JSON.stringify(event));
+      console.log('Reached the job:ready event callback');
 
       // Destructure the 'context' object from the event object to get the necessary IDs
       const { spaceId, environmentId, jobId } = event.context;
+
+      const space = await api.spaces.get(spaceId);
+
+      console.log('Space: ' + JSON.stringify(space));
+
+      const metadata = space.data.metadata as Metadata;
+
+      const userId = metadata.userId;
+
+      const updateJob1 = await api.jobs.ack(jobId, {
+        info: 'Creating Space',
+        progress: 10,
+      });
+
+      console.log('Updated Job: ' + JSON.stringify(updateJob1));
 
       // Log the environment ID to the console
       console.log('env: ' + environmentId);
       console.log('spaceId ' + spaceId);
       console.log('jobID: ' + jobId);
 
-      // Create a new workbook using the Flatfile API
-      const createWorkbook = await api.workbooks.create({
-        spaceId: spaceId,
-        environmentId: environmentId,
-        labels: ['primary'],
-        name: 'Benefits Workbook',
-        sheets: blueprintSheets,
-        actions: [
-          {
-            operation: 'submitAction',
-            slug: 'HCMWorkbookSubmitAction',
-            mode: 'foreground',
-            label: 'Submit',
-            type: 'string',
-            description: 'Submit Data to HCM Show',
-            primary: true,
-          },
-        ],
+      try {
+        // Create a new workbook using the Flatfile API
+        const createWorkbook = await api.workbooks.create({
+          spaceId: spaceId,
+          environmentId: environmentId,
+          labels: ['primary'],
+          name: 'Benefits Workbook',
+          sheets: blueprintSheets,
+          actions: [
+            {
+              operation: 'submitAction',
+              slug: 'HCMWorkbookSubmitAction',
+              mode: 'foreground',
+              label: 'Submit',
+              type: 'string',
+              description: 'Submit Data to HCM Show',
+              primary: true,
+            },
+          ],
+        });
+
+        const workbookId = createWorkbook.data.id;
+        if (workbookId) {
+          console.log('Created Workbook with ID:' + workbookId);
+
+          // Update the space to set the primary workbook and theme using api.spaces.update
+          const updatedSpace = await api.spaces.update(spaceId, {
+            environmentId: environmentId,
+            primaryWorkbookId: workbookId,
+            metadata: {
+              userId,
+              sidebarConfig: {
+                showSidebar: false,
+                // This property seems to break guest magic link functionality?
+                // showGuestInvite: true,
+              },
+              theme: {
+                root: {
+                  primaryColor: '#090B2B',
+                  dangerColor: 'salmon',
+                  warningColor: 'gold',
+                },
+                sidebar: {
+                  logo: `https://images.ctfassets.net/e8fqfbar73se/4c9ouGKgET1qfA4uxp4qLZ/e3f1a8b31be67a798c1e49880581fd3d/white-logo-w-padding.png`,
+                  textColor: 'white',
+                  titleColor: 'white',
+                  focusBgColor: '#616A7D',
+                  focusTextColor: 'white',
+                  backgroundColor: '#090B2B',
+                  footerTextColor: 'white',
+                  textUltralightColor: 'red',
+                },
+                table: {
+                  inputs: {
+                    radio: {
+                      color: 'rgb(8 117 225)',
+                    },
+                    checkbox: {
+                      color: 'rgb(8 117 225)',
+                    },
+                  },
+                  filters: {
+                    color: 'gray',
+                    active: {
+                      backgroundColor: 'rgb(8 117 225)',
+                    },
+                    error: {
+                      activeBackgroundColor: 'salmon',
+                    },
+                  },
+                  column: {
+                    header: {
+                      fontSize: '12px',
+                      backgroundColor: 'rgb(240 240 240)',
+                      color: 'slategray',
+                      dragHandle: {
+                        idle: 'rgb(8 117 225)',
+                        dragging: 'blue',
+                      },
+                    },
+                  },
+                  fontFamily: 'Arial',
+                  indexColumn: {
+                    backgroundColor: 'rgb(240 240 240)',
+                    selected: {
+                      color: 'rgb(240 240 240)',
+                      backgroundColor: 'rgb(200 200 200)',
+                    },
+                  },
+                  cell: {
+                    selected: {
+                      backgroundColor: 'rgb(235 245 255)',
+                    },
+                    active: {
+                      borderColor: 'rgb(8 117 225)',
+                      spinnerColor: 'gray',
+                    },
+                  },
+                  boolean: {
+                    toggleChecked: 'rgb(240 240 240)',
+                  },
+                  loading: {
+                    color: 'rgb(240 240 240)',
+                  },
+                },
+              },
+            },
+          });
+
+          // Log the ID of the updated space to the console
+          console.log('Updated Space with ID: ' + updatedSpace.data.id);
+        } else {
+          console.log('Unable to retrieve workbook ID from the response.');
+        }
+      } catch (error) {
+        console.log('Error creating workbook or updating space:', error);
+      }
+
+      const createDoc = await api.documents.create(spaceId, {
+        title: 'Welcome',
+        body: `<div> 
+        <h1 style="margin-bottom: 36px;">Welcome! We're excited to help you import your data to HCM Show.</h1>
+        <h2 style="margin-top: 0px; margin-bottom: 12px;">Follow the steps below to get started:</h2>
+        <h2 style="margin-bottom: 0px;">1. Upload your file</h2>
+        <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
+        <h2 style="margin-bottom: 0px;">2. Import the Benefit Elections Data</h2>
+        <p style="margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the benefit elections data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
+        <h2 style="margin-bottom: 0px;">3. Validate and Transform Data</h2>
+        <p style="margin-top: 0px; margin-bottom: 8px;">Make sure to verify that your data is correctly formatted and transformed by Flatfile. Flatfile will handle formatting dates, rounding amounts, and validating the existence of employees and benefit plans for you! If there are any issues or errors, you can easily address them within Flatfile's user interface.</p>
+        <h2 style="margin-bottom: 0px;">4. Load Data into HCM.Show</h2>
+        <p style="margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the “Push records to HCM.show” button to load data into the HCM.Show application.</p>
+        <h2 style="margin-bottom: 0px;">5. Return to HCM.Show</h2>
+        <p style="margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the benefit elections data that you have just loaded.</p>
+        <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+      </div>`,
       });
 
-      const workbookId = createWorkbook.data.id;
-      // Log the result of the createWorkbook function to the console as a string
-      console.log('Created Workbook with ID: ' + workbookId);
-
-      const space = await api.spaces.get(spaceId);
-
-      console.log('Space: ' + JSON.stringify(space));
-
-      const userId = (space.data.metadata as { userId: string }).userId;
-
-      // Update Space to set primary workbook for data checklist functionality using the Flatfile API
-      const updateSpace = await api.spaces.update(spaceId, {
-        environmentId: environmentId,
-        primaryWorkbookId: workbookId,
-        metadata: {
-          userId,
-        },
-      });
-      // Log the result of the updateSpace function to the console as a string
-      console.log('Updated Space with ID: ' + updateSpace.data.id);
+      console.log('Created Document: ' + createDoc);
 
       // Update the job status to 'complete' using the Flatfile API
       const updateJob = await api.jobs.update(jobId, {
@@ -96,6 +204,11 @@ export default function (listener) {
       console.log(
         'Updated Job With ID to Status Complete: ' + updateJob.data.id
       );
+    });
+
+    // Handle the 'job:failed' event
+    configure.on('job:failed', async (event) => {
+      console.log('Job Failed: ' + JSON.stringify(event));
     });
   });
 
