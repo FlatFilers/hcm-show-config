a3dc749d0979cbe7815e31b11ae91fe62ac20336
John Mosesman <johnmosesman@gmail.com>
2023-06-21 14:22:51 -0500

    Script to keep track of all the env for prod and develop, and work with them without manually updating them.

---
+++ scripts/deploy/.example-workflow-envs.ts
@@ -0,0 +1,38 @@
+export default {
+  production: {
+    onboarding: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    filefeed: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    embedded: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    dynamic: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+  },
+  dev: {
+    onboarding: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    filefeed: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    embedded: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+    dynamic: {
+      apiKey: 'sk_...',
+      environmentId: 'us_env_...',
+    },
+  },
+};
