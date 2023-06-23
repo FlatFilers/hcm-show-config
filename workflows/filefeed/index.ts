920e91fa9ec0ec25309c582b75088021b124d0a9
Colin Frederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-23 18:36:13 -0500

    Update index.ts
    - Updated verbiage based on info from Slack. https://flatfile.slack.com/archives/C04H7V10LKX/p1687548396095279?thread_ts=1687539056.820619&cid=C04H7V10LKX

--- workflows/filefeed/index.ts
+++ workflows/filefeed/index.ts
@@ -182,9 +182,9 @@ export default function (listener) {
     <h2 style="margin-bottom: 0px;">1. Inspect the Automatically Uploaded File</h2>
     <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar. Here, you can view the original file that was automatically imported into Flatfile from Google Drive.</p>
     <h2 style="margin-bottom: 0px;">2. Examine the Imported Benefit Elections Data</h2>
-    <p style="margin-top: 0px; margin-bottom: 8px;">Click on the "Benefit Elections" workbook in the left-hand sidebar. This will display the data that has been mapped and transformed from the Google Drive file.</p>
+    <p style="margin-top: 0px; margin-bottom: 8px;">Click on the "Benefit Elections" workbook in the left-hand sidebar. This will display the data the remaining invalid values from the Google Drive file.</p>
     <h2 style="margin-bottom: 0px;">3. Confirm the Data Load into HCM.Show</h2>
-    <p style="margin-top: 0px; margin-bottom: 12px;">As part of this process, we've automatically uploaded a file, mapped the data, and loaded this data into HCM Show. All you need to do is confirm it.</p>
+    <p style="margin-top: 0px; margin-bottom: 12px;">As part of this process, we've automatically uploaded a file, mapped the data, and loaded all valid records into HCM Show.</p>
     <h2 style="margin-bottom: 0px;">4. Return to HCM.Show</h2>
     <p style="margin-top: 0px; margin-bottom: 36px;">Once you have inspected the files and data in Flatfile, return to HCM.Show. Navigate to the Data Templates section within the application to view the benefit elections data that was just loaded.</p>
     <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
