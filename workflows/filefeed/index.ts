cc88f6f9689ed58219bdb599efa02131b1bd01ab
Colin Frederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-06-26 11:39:54 -0500

    Update index.ts (#55)
    * Update index.ts
    
    - Updates to verbiage for filefeed Welcome page
    
    * Update index.ts
    
    - Updated verbiage based on info from Slack. https://flatfile.slack.com/archives/C04H7V10LKX/p1687548396095279?thread_ts=1687539056.820619&cid=C04H7V10LKX

--- workflows/filefeed/index.ts
+++ workflows/filefeed/index.ts
@@ -178,20 +178,18 @@ export default function (listener) {
       const createDoc = await api.documents.create(spaceId, {
         title: 'Welcome',
         body: `<div> 
-        <h1 style="margin-bottom: 36px;">Welcome! We're excited to help you import your data to HCM Show.</h1>
-        <h2 style="margin-top: 0px; margin-bottom: 12px;">Follow the steps below to get started:</h2>
-        <h2 style="margin-bottom: 0px;">1. Upload your file</h2>
-        <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
-        <h2 style="margin-bottom: 0px;">2. Import the Benefit Elections Data</h2>
-        <p style="margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the benefit elections data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
-        <h2 style="margin-bottom: 0px;">3. Validate and Transform Data</h2>
-        <p style="margin-top: 0px; margin-bottom: 8px;">Make sure to verify that your data is correctly formatted and transformed by Flatfile. Flatfile will handle formatting dates, rounding amounts, and validating the existence of employees and benefit plans for you! If there are any issues or errors, you can easily address them within Flatfile's user interface.</p>
-        <h2 style="margin-bottom: 0px;">4. Load Data into HCM.Show</h2>
-        <p style="margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the “Push records to HCM.show” button to load data into the HCM.Show application.</p>
-        <h2 style="margin-bottom: 0px;">5. Return to HCM.Show</h2>
-        <p style="margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the benefit elections data that you have just loaded.</p>
-        <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
-      </div>`,
+    <h1 style="margin-bottom: 36px;">Welcome! We're excited to offer you a seamless, one-click experience for loading your data into HCM Show.</h1>
+    <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
+    <h2 style="margin-bottom: 0px;">1. Inspect the Automatically Uploaded File</h2>
+    <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar. Here, you can view the original file that was automatically imported into Flatfile from Google Drive.</p>
+    <h2 style="margin-bottom: 0px;">2. Examine the Imported Benefit Elections Data</h2>
+    <p style="margin-top: 0px; margin-bottom: 8px;">Click on the "Benefit Elections" workbook in the left-hand sidebar. This will display the data the remaining invalid values from the Google Drive file.</p>
+    <h2 style="margin-bottom: 0px;">3. Confirm the Data Load into HCM.Show</h2>
+    <p style="margin-top: 0px; margin-bottom: 12px;">As part of this process, we've automatically uploaded a file, mapped the data, and loaded all valid records into HCM Show.</p>
+    <h2 style="margin-bottom: 0px;">4. Return to HCM.Show</h2>
+    <p style="margin-top: 0px; margin-bottom: 36px;">Once you have inspected the files and data in Flatfile, return to HCM.Show. Navigate to the Data Templates section within the application to view the benefit elections data that was just loaded.</p>
+    <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+</div>`,
       });
 
       console.log('Created Document: ' + createDoc);
