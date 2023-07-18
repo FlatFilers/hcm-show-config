6f5f15ccc5470b2dd54d690a3da301f5a24ef120
Jason Gong <jasongong83@gmail.com>
2023-07-18 14:29:25 -0400

    add callback with conditional to stall completion

--- workflows/project/index.ts
+++ workflows/project/index.ts
@@ -363,9 +363,11 @@ export default function (listener) {
           progress: 10,
         });
 
+        let callback;
         try {
           // Call the submit function with the event as an argument to push the data to HCM Show
-          await pushToHcmShow(event);
+          callback = await pushToHcmShow(event);
+
           // Log the action as a string to the console
           console.log('Action: ' + JSON.stringify(event?.payload?.operation));
         } catch (error) {
@@ -374,9 +376,11 @@ export default function (listener) {
           // Perform error handling, such as displaying an error message to the user or triggering a fallback behavior
         }
 
-        await api.jobs.complete(jobId, {
-          info: 'Data synced to the HCM.show app.',
-        });
+        if (callback === '{"success":true}') {
+          await api.jobs.complete(jobId, {
+            info: 'Data synced to the HCM.show app.',
+          });
+        }
       } catch (error) {
         console.error('Error:', error.stack);
 
