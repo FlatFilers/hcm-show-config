eb8209b2a45df6278e694e0fbfe4ea0eb57b7cdf
John Mosesman <johnmosesman@users.noreply.github.com>
2023-07-31 16:24:44 -0500

    Use secrets for auth on API call (#77)

--- common/hcm-show-api-service.ts
+++ common/hcm-show-api-service.ts
@@ -1,3 +1,4 @@
+import { FlatfileEvent } from '@flatfile/listener';
 import axios from 'axios';
 
 type DepartmentResult = {
@@ -10,11 +11,17 @@ export class HcmShowApiService {
   static BASE_URL = 'https://hcm.show';
   // static BASE_URL = 'https://206d-205-185-214-250.ngrok-free.app';
 
-  static fetchDepartments = async (): Promise<DepartmentResult[]> => {
+  static fetchDepartments = async (
+    event: FlatfileEvent
+  ): Promise<DepartmentResult[]> => {
     let response;
 
     try {
-      response = await axios.get(`${this.BASE_URL}/api/v1/departments`);
+      response = await axios.get(`${this.BASE_URL}/api/v1/departments`, {
+        headers: {
+          'x-server-auth': await event.secrets('SERVER_AUTH_TOKEN'),
+        },
+      });
     } catch (error) {
       console.error(
         'Error fetching departments from HCM.show: ' + JSON.stringify(error)
