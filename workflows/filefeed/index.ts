56d6655070d493db1b307670f78f7b26536aff2a
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-08-22 11:32:11 -0600

    Landing Page Updates
    - Updates landing pages on all workflows to the document created during space creation.

--- workflows/filefeed/index.ts
+++ workflows/filefeed/index.ts
@@ -46,6 +46,27 @@ export default function (listener) {
       console.log('spaceId ' + spaceId);
       console.log('jobID: ' + jobId);
 
+      const createDoc = await api.documents.create(spaceId, {
+        title: 'Welcome',
+        body: `<div> 
+    <h1 style="margin-bottom: 36px;">Welcome! We're excited to offer you a seamless, one-click experience for loading your data into HCM Show.</h1>
+    <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
+    <h2 style="margin-bottom: 0px;">1. Inspect the Automatically Uploaded File</h2>
+    <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar. Here, you can view the original file that was automatically imported into Flatfile from Google Drive.</p>
+    <h2 style="margin-bottom: 0px;">2. Examine the Imported Benefit Elections Data</h2>
+    <p style="margin-top: 0px; margin-bottom: 8px;">Click on the "Benefit Elections" workbook in the left-hand sidebar. This will display the data the remaining invalid values from the Google Drive file.</p>
+    <h2 style="margin-bottom: 0px;">3. Confirm the Data Load into HCM.Show</h2>
+    <p style="margin-top: 0px; margin-bottom: 12px;">As part of this process, we've automatically uploaded a file, mapped the data, and loaded all valid records into HCM Show.</p>
+    <h2 style="margin-bottom: 0px;">4. Return to HCM.Show</h2>
+    <p style="margin-top: 0px; margin-bottom: 36px;">Once you have inspected the files and data in Flatfile, return to HCM.Show. Navigate to the Data Templates section within the application to view the benefit elections data that was just loaded.</p>
+    <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+</div>`,
+      });
+
+      console.log('Created Document: ' + createDoc);
+
+      const documentId = createDoc.data.id;
+
       try {
         // Create a new workbook using the Flatfile API
         const createWorkbook = await api.workbooks.create({
@@ -79,6 +100,9 @@ export default function (listener) {
               userId,
               sidebarConfig: {
                 showSidebar: true,
+                defaultPage: {
+                  documentId,
+                },
                 // This property seems to break guest magic link functionality?
                 // showGuestInvite: true,
               },
@@ -162,25 +186,6 @@ export default function (listener) {
       } catch (error) {
         console.log('Error creating workbook or updating space:', error);
       }
-
-      const createDoc = await api.documents.create(spaceId, {
-        title: 'Welcome',
-        body: `<div> 
-    <h1 style="margin-bottom: 36px;">Welcome! We're excited to offer you a seamless, one-click experience for loading your data into HCM Show.</h1>
-    <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
-    <h2 style="margin-bottom: 0px;">1. Inspect the Automatically Uploaded File</h2>
-    <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar. Here, you can view the original file that was automatically imported into Flatfile from Google Drive.</p>
-    <h2 style="margin-bottom: 0px;">2. Examine the Imported Benefit Elections Data</h2>
-    <p style="margin-top: 0px; margin-bottom: 8px;">Click on the "Benefit Elections" workbook in the left-hand sidebar. This will display the data the remaining invalid values from the Google Drive file.</p>
-    <h2 style="margin-bottom: 0px;">3. Confirm the Data Load into HCM.Show</h2>
-    <p style="margin-top: 0px; margin-bottom: 12px;">As part of this process, we've automatically uploaded a file, mapped the data, and loaded all valid records into HCM Show.</p>
-    <h2 style="margin-bottom: 0px;">4. Return to HCM.Show</h2>
-    <p style="margin-top: 0px; margin-bottom: 36px;">Once you have inspected the files and data in Flatfile, return to HCM.Show. Navigate to the Data Templates section within the application to view the benefit elections data that was just loaded.</p>
-    <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
-</div>`,
-      });
-
-      console.log('Created Document: ' + createDoc);
 
       // Update the job status to 'complete' using the Flatfile API
       const updateJob = await api.jobs.update(jobId, {
