48586e454e14393a5dbac6f43e9247f50cd3bb97
John Mosesman <johnmosesman@gmail.com>
2023-12-07 16:27:15 -0600

    save

--- workflows/project/document.ts
+++ workflows/project/document.ts
@@ -2,25 +2,39 @@ export const document = {
   title: 'Welcome',
   body: `<div class="my-doc"> 
 <style>
-  .my-doc {
-    margin: 24px;
-  }
-
-  .my-doc h1, .my-doc h2, .my-doc p {
-    padding: 0px;
-    margin: 0px;
-  }
-
-  .box {
-    border: 1px solid #17B9A7;
-    padding: 8px;
-    margin-bottom: 12px;
-    border-radius: 8px;
-    box-shadow: 4px 4px 20px 0px #0000001F;
-    padding: 24px;
-  }
+.my-doc {
+  margin: 24px;
+}
+.my-doc h1 {
+  padding: 0px;
+  margin: 0px;
+}
+.my-doc h2 {
+  padding: 0px;
+  margin: 0px;
+}
+.my-doc p {
+  padding: 0px;
+  margin: 0px;
+}
+.box {
+  border: 1px solid #17B9A7;
+  padding: 8px;
+  margin-bottom: 12px;
+  border-radius: 8px;
+  box-shadow: 4px 4px 20px 0px #0000001F;
+  padding: 24px;
+}
+.box h2 {
+  font-size: 16px; 
+  font-weight: 600;
+}
+.box p {
+  font-size: 14px;
+}
 </style>
 
+<div>
 <div style="margin-bottom: 36px;">
 <h1>Welcome!</h1>
 <p style="font-weight: 600;">We're excited to help you import your data to HCM Show.</p>
@@ -32,35 +46,46 @@ export const document = {
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">1. Upload your file</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
+<h2>1. Upload your file</h2>
+<p>Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">2. Import your Jobs Data</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Jobs data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
+<h2>2. Import your Jobs Data</h2>
+<p>Click "Import" and select the Jobs data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">3. Import your Employee Data</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Employee data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
+<h2>3. Import your Employee Data</h2>
+<p>Click "Import" and select the Employee data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">4. Validate and Transform Data</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Ensure that the data is correctly formatted and transformed By Flatfile. You can easily address any issues and errors within Flatfile's user interface.</p>
+<h2>4. Validate and Transform Data</h2>
+<p>Ensure that the data is correctly formatted and transformed By Flatfile. You can easily address any issues and errors within Flatfile's user interface.</p>
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">5. Load Data into HCM.Show</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the custom action on each sheet to load the data into HCM.Show application.</p>
+<h2>5. Load Data into HCM.Show</h2>
+<p>Once the data has been validated and transformed, use the custom action on each sheet to load the data into HCM.Show application.</p>
 </div>
 
 <div class="box">
-<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">6. Return to HCM.Show</h2>
-<p style="font-size: 14px; margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the Jobs and Employees data that you have just loaded.</p>
+<h2>6. Return to HCM.Show</h2>
+<p>Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the Jobs and Employees data that you have just loaded.</p>
 </div>
 
-<h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+<h3>Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+</div>
+
+<div>
+<p>Flatfile features covered:</p>
+<div style="border: 1px solid #000; margin: 18px 0px;"></div>
+
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="text-gray-400 mr-2 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path></svg>
+<p>Event-based workflow</p>
+</div>
+
 </div>`,
 };
