31d12929eb5e593db3c593e74604a7ac538f2f59
John Mosesman <johnmosesman@gmail.com>
2023-12-07 16:17:38 -0600

    Wip

--- workflows/project/document.ts
+++ workflows/project/document.ts
@@ -1,20 +1,66 @@
 export const document = {
   title: 'Welcome',
-  body: `<div> 
-              <h1 style="margin-bottom: 36px;">Welcome! We're excited to help you import your data to HCM Show.</h1>
-              <h2 style="margin-top: 0px; margin-bottom: 12px;">Follow the steps below to get started:</h2>
-              <h2 style="margin-bottom: 0px;">1. Upload your file</h2>
-              <p style="margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
-              <h2 style="margin-bottom: 0px;">2. Import your Jobs Data</h2>
-              <p style="margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Jobs data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
-              <h2 style="margin-bottom: 0px;">3. Import your Employee Data</h2>
-              <p style="margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Employee data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
-              <h2 style="margin-bottom: 0px;">4. Validate and Transform Data</h2>
-              <p style="margin-top: 0px; margin-bottom: 8px;">Ensure that the data is correctly formatted and transformed By Flatfile. You can easily address any issues and errors within Flatfile's user interface.</p>
-              <h2 style="margin-bottom: 0px;">5. Load Data into HCM.Show</h2>
-              <p style="margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the custom action on each sheet to load the data into HCM.Show application.</p>
-              <h2 style="margin-bottom: 0px;">6. Return to HCM.Show</h2>
-              <p style="margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the Jobs and Employees data that you have just loaded.</p>
-              <h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
-            </div>`,
+  body: `<div class="my-doc"> 
+<style>
+  .my-doc {
+    margin: 24px;
+  }
+
+  .my-doc h1, .my-doc h2, .my-doc p {
+    padding: 0px;
+    margin: 0px;
+  }
+
+  .box {
+    border: 1px solid #17B9A7;
+    padding: 8px;
+    margin-bottom: 12px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 20px 0px #0000001F;
+    padding: 24px;
+  }
+</style>
+
+<div style="margin-bottom: 36px;">
+<h1>Welcome!</h1>
+<p style="font-weight: 600;">We're excited to help you import your data to HCM Show.</p>
+</div>
+
+<div>
+<p style="">Follow the steps below to get started:</p>
+<div style="margin: 18px 0px; border: 1px solid #000;"></div>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">1. Upload your file</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Files" in the left-hand sidebar, and upload the sample data you want to import into Flatfile. You can do this by clicking "Add files" or dragging and dropping the file onto the page.</p>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">2. Import your Jobs Data</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Jobs data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">3. Import your Employee Data</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Click "Import" and select the Employee data. Follow the mapping instructions in Flatfile to complete the import. Once the data has been mapped, it will be loaded into Flatfile's table UI, where validations and transformations have been applied.</p>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">4. Validate and Transform Data</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 8px;">Ensure that the data is correctly formatted and transformed By Flatfile. You can easily address any issues and errors within Flatfile's user interface.</p>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">5. Load Data into HCM.Show</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 12px;">Once the data has been validated and transformed, use the custom action on each sheet to load the data into HCM.Show application.</p>
+</div>
+
+<div class="box">
+<h2 style="font-size: 16px; margin-bottom: 0px; font-weight: 600;">6. Return to HCM.Show</h2>
+<p style="font-size: 14px; margin-top: 0px; margin-bottom: 36px;">Once you have loaded the data from Flatfile to HCM Show, return to HCM.Show and navigate to the Data Templates section within the application to view the Jobs and Employees data that you have just loaded.</p>
+</div>
+
+<h3 style="margin-top: 0px; margin-bottom: 12px;">Remember, if you need any assistance, you can always refer back to this page by clicking "Welcome" in the left-hand sidebar!</h3>
+</div>`,
 };
